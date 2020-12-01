e89b12ec9e16228a94830a9485e20b6a0d3e6fa8
DCM <dcmay@ualberta.ca>
2020-12-01 14:55:20 -0700

    autocorrelation and plotting

--- analysis.py
+++ analysis.py
@@ -4,40 +4,56 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from importUtils import nigel_data_to_pd, thomas_data_to_pd
-
-def somerandomanalysis(crop=True):
+def import_both_same_timesteps():
     nigel_nwp = nigel_data_to_pd()
     print('Nigel nwp: ', nigel_nwp.columns)
     thomas_nwp = thomas_data_to_pd()
     print('Thomas columns: ', thomas_nwp.columns)
 
-    if crop:
-        nigel_nwp = nigel_nwp[nigel_nwp['Time (UTC)'] > min(thomas_nwp['Time'])]
-        nigel_nwp = nigel_nwp[nigel_nwp['Time (UTC)'] < max(thomas_nwp['Time'])]
+    nigel_nwp = nigel_nwp[nigel_nwp['Time (UTC)'] > min(thomas_nwp['Time'])]
+    nigel_nwp = nigel_nwp[nigel_nwp['Time (UTC)'] < max(thomas_nwp['Time'])]
 
-    steps = 5000
+    return nigel_nwp, thomas_nwp
 
-    plt.title('ShortWaveDown')
-    plt.plot(nigel_nwp['Time (UTC)'][:steps], nigel_nwp['Short_Wave_Flux_Down [W/m2]'][:steps], label='Nigel')
-    plt.plot(thomas_nwp['Time'][:steps], thomas_nwp['SWDOWN'][:steps], label='Thomas')
-    plt.legend()
-    plt.show()
 
-    plt.title('Temp')
-    plt.plot(nigel_nwp['Time (UTC)'][:steps], nigel_nwp['Temperature [K]'][:steps], label='Nigel')
-    plt.plot(thomas_nwp['Time'][:steps], thomas_nwp['T2'][:steps], label='Thomas')
-    plt.legend()
-    plt.show()
+def crosscorrelate_and_plot():
+    nigel_nwp, thomas_nwp = import_both_same_timesteps()
 
-    plt.title('Rain(1?)')
-    plt.plot(nigel_nwp['Time (UTC)'][:steps], nigel_nwp['Precipitation [mm]'][:steps], label='Nigel')
-    plt.plot(thomas_nwp['Time'][:steps], thomas_nwp['RAINC'][:steps], label='Thomas')
-    plt.legend()
-    plt.show()
+    all_nwp = pd.merge(thomas_nwp, nigel_nwp, how='inner', left_on='Time', right_on='Time (UTC)')
+    thomas_nwp = all_nwp[thomas_nwp.columns]
+    nigel_nwp = all_nwp[nigel_nwp.columns]
 
-    plt.title('Rain(2?)')
-    plt.plot(nigel_nwp['Time (UTC)'][:steps], nigel_nwp['Precipitation [mm]'][:steps], label='Nigel')
-    plt.plot(thomas_nwp['Time'][:steps], thomas_nwp['RAINNC'][:steps], label='Thomas')
-    plt.legend()
-    plt.show()
+    coeffs = np.zeros(shape=[len(thomas_nwp.columns), len(nigel_nwp.columns)])
+
+    x=0
+    for thomas_variable in thomas_nwp.columns:
+        thomas_var_np = thomas_nwp[thomas_variable].to_numpy()
+
+        y=0
+        for nigel_variable in nigel_nwp.columns:
+            nigel_var_np = nigel_nwp[nigel_variable].to_numpy()
+
+            xcoeff =  np.corrcoef(thomas_var_np, nigel_var_np)
+            coeffs[x,y] = xcoeff[0,1]
+            y += 1
+        x += 1
+
+    max_xs = np.argmax(coeffs, axis=-1)
+    for x in range(len(thomas_nwp.columns)):
+        thomas_variable = thomas_nwp.columns[x]
+        corresponding_nigel_variable  = nigel_nwp.columns[max_xs[x]]
+
+
+        steps = 5000
+        corr = coeffs[x, max_xs[x]]
+        print(corr)
+
+        plt.title('corrcoeeff: ' + str(corr))
+        plt.plot(nigel_nwp['Time (UTC)'][:steps], nigel_nwp[corresponding_nigel_variable][:steps], label='Nigel: ' + corresponding_nigel_variable )
+        plt.plot(thomas_nwp['Time'][:steps], thomas_nwp[thomas_variable][:steps], label='Thomas: ' + thomas_variable)
+        plt.legend()
+        plt.show()
+
+
+
 
